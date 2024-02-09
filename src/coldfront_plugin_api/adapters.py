7ad5cc9a0d749fadedeed63bfef0ff6c3bffeb34
Quan Pham <qmpham2019@gmail.com>
2024-02-09 15:35:07 -0500

    Adapters.py: Override/Implement to_dict function for users

--- src/coldfront_plugin_api/adapters.py
+++ src/coldfront_plugin_api/adapters.py
@@ -3,6 +3,33 @@ Defines the SCIM adapter for coldfront users and groups
 '''
 
 from django_scim.adapters import SCIMUser, SCIMGroup
+from django_scim import constants
 
 class SCIMColdfrontUser(SCIMUser):
     id_field = "id"
+
+    def to_dict(self):
+
+        d = {
+            'schemas': [constants.SchemaURI.USER],
+            "id": self.obj.username,
+            "externalId": self.obj.username,
+            "userName": self.obj.username,
+            "name": {
+                "givenName": self.obj.first_name,
+                "familyName": self.obj.last_name,
+            },
+            "emails": [
+                {
+                    "value": self.obj.email,
+                    "type": "work",
+                    "primary": True,
+                }
+            ],
+            "meta": {
+                "resourceType": "User",
+                "created": self.obj.date_joined,
+            },
+        }
+
+        return d
