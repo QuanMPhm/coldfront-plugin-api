870c95647f22da6801492ec530bb9ad5506e4048
Quan Pham <qmpham2019@gmail.com>
2024-02-12 13:07:25 -0500

    Adapters.py: Override from_dict function to implement SCIM POST of users

--- src/coldfront_plugin_api/scim_v2/adapters.py
+++ src/coldfront_plugin_api/scim_v2/adapters.py
@@ -5,6 +5,8 @@ Defines the SCIM adapter for coldfront users and groups
 from django_scim.adapters import SCIMUser, SCIMGroup
 from django_scim import constants
 
+from coldfront_plugin_api import auth, utils
+
 class SCIMColdfrontUser(SCIMUser):
     id_field = "id"
 
@@ -33,3 +35,27 @@ class SCIMColdfrontUser(SCIMUser):
         }
 
         return d
+
+    def from_dict(self, d):
+
+        '''
+        Overide from SCIMUser, this function first check if user exists through one of 
+        the configured search providers, and if yes, will fetch additional information from there.
+
+        Otherwise, for setting the attributes manually from the request's payload
+        '''
+        username = d.get('userName')
+        found = utils.find_user(username) # Fetch user
+
+        # If the user is not found in the search provider,
+        # allow the API client to specify the values that would have
+        # otherwise been fetched.
+        # This allows preregistering a user that doesn't have a corresponding
+        # identity provider yet.
+        if found:
+            self.obj.username = username
+            self.obj.first_name = found["first_name"]
+            self.obj.last_name = found["last_name"]
+            self.obj.email = found["email"]
+        else:
+            super().from_dict(d)
