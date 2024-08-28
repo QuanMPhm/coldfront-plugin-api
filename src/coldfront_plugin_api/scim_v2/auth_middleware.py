bafbdda02ac103269977732ce35702286bb4713c
Quan Pham <qmpham2019@gmail.com>
2024-08-28 13:57:28 -0400

    Added functional testing for SCIM authentication

--- src/coldfront_plugin_api/scim_v2/auth_middleware.py
+++ src/coldfront_plugin_api/scim_v2/auth_middleware.py
@@ -10,8 +10,9 @@ logger = logging.getLogger(__name__)
 class SCIMColdfrontAuthCheckMiddleware(SCIMAuthCheckMiddleware):
     def process_request(self, request):
         if not request.user or not request.user.is_authenticated:
-            # PLUGIN_AUTH_OIDC implies DRF OIDC backend is configured
+            # Check if OIDC authentication is enabled to see if we should authenticate user using OIDC
+            # PLUGIN_AUTH_OIDC implies OIDC is enabled and DRF OIDC backend is configured
             if os.getenv("PLUGIN_AUTH_OIDC") == "True":
-                oidc_auth_obj = OIDCAuthentication()
-                request.user = oidc_auth_obj.authenticate(request)
+                if user_tuple := OIDCAuthentication().authenticate(request):
+                    request.user = user_tuple[0]
         return super().process_request(request)
